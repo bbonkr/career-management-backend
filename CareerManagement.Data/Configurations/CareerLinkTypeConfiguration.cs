0b02dccbc63b59bb6a6359d367df33696143e190
Pon Cheol Ku <bbon@bbon.kr>
2020-03-17 17:39:05 +0900

    데이터 컨텍스트 추가

---
+++ CareerManagement.Data/Configurations/CareerLinkTypeConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using CareerManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CareerManagement.Data.Configurations
+{
+    public class CareerLinkTypeConfiguration : IEntityTypeConfiguration<CareerLink>
+    {
+        public void Configure(EntityTypeBuilder<CareerLink> b)
+        {
+            b.ToTable("CareerLink");
+            b.HasKey(x => new { x.CareerId, x.LinkId });
+
+            b
+                .Property(x => x.CareerId)
+                .IsRequired()
+                .HasMaxLength(StringLength.Identifier)
+                .HasComment("경력 식별자")
+                ;
+
+            b
+                .Property(x => x.LinkId)
+                .IsRequired()
+                .HasMaxLength(StringLength.Identifier)
+                .HasComment("링크 식별자")
+                ;
+
+            b
+                .HasOne(x => x.Career)
+                .WithMany(x => x.Links)
+                .HasForeignKey(x => x.CareerId);
+
+            b
+                .HasOne(x => x.Link)
+                .WithMany(x => x.Careers)
+                .HasForeignKey(x => x.LinkId);
+        }
+    }
+}
---
+++ CareerManagement.Data/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using CareerManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CareerManagement.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Career> Careers { get; set; }
+
+        public DbSet<Education> Educations { get; set; }
+
+        public DbSet<Portfolio> Portfolios { get; set; }
+
+        public DbSet<Profile> Profiles { get; set; }
+
+        public DbSet<Tech> Teches { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+    }
+}
