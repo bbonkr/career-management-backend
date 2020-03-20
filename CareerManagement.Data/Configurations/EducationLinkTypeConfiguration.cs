da6578cfa9240a8b76324c81e7fa5c595240a3ae
Pon Cheol Ku <bbon@bbon.kr>
2020-03-20 18:07:59 +0900

    모델 구성

---
+++ CareerManagement.Data/Configurations/EducationLinkTypeConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using CareerManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CareerManagement.Data.Configurations
+{
+    public class EducationLinkTypeConfiguration : IEntityTypeConfiguration<EducationLink>
+    {
+        public void Configure(EntityTypeBuilder<EducationLink> b)
+        {
+            b.ToTable("EducationLink");
+            b.HasKey(x => new { x.EducationId, x.LinkId });
+
+            b.Property(x => x.EducationId)
+                .IsRequired()
+                .HasMaxLength(StringLength.Identifier)
+                .HasComment("교육 식별자")
+                ;
+
+            b.Property(x => x.LinkId)
+                .IsRequired()
+                .HasMaxLength(StringLength.Identifier)
+                .HasComment("링크 식별자");
+
+            b
+                .HasOne(x => x.Education)
+                .WithMany(x => x.Links)
+                .HasForeignKey(x => x.EducationId)
+                .OnDelete(DeleteBehavior.Cascade);
+            b
+                .HasOne(x => x.Link)
+                .WithMany(x => x.Educations)
+                .HasForeignKey(x => x.LinkId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
---
+++ CareerManagement.Data/Configurations/EducationTypeConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using CareerManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CareerManagement.Data.Configurations
+{
+    public class EducationTypeConfiguration : IEntityTypeConfiguration<Education>
+    {
+        public void Configure(EntityTypeBuilder<Education> b)
+        {
+            b.ToTable("Education");
+
+            b.HasKey(x => x.Id);
+
+            b.Property(x => x.Id)
+                .IsRequired()
+                .HasMaxLength(StringLength.Identifier)
+                .HasComment("식별자")
+                ;
+
+            b
+                .Property(x => x.UserId)
+                .IsRequired()
+                .HasMaxLength(StringLength.Identifier)
+                .HasComment("사용자 식별자")
+                ;
+
+            b.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(StringLength.Name)
+                .HasComment("제목")
+                ;
+
+            b.Property(x => x.State)
+                .IsRequired(false)
+                .HasMaxLength(StringLength.Name)
+                .HasComment("상태")
+                ;
+
+            b.Property(x => x.Description)
+                .IsRequired(false)
+                .HasMaxLength(StringLength.Note)
+                .HasComment("설명")
+                ;
+
+            b.HasOne(x => x.User).WithMany(x => x.Educations).HasForeignKey(x => x.UserId);
+            b.HasMany(x => x.Links).WithOne(x => x.Education);
+        }
+    }
+}
